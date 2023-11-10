3f2adb8b871d6f39b2eb5e5c343780abdb078453
Arthur Griffith <arthur.griffith94@gmail.com>
2023-11-09 20:12:14 -0500

    added data flow to overview page

--- app/styles.ts
+++ app/styles.ts
@@ -1,6 +1,8 @@
 const styles = {
-  header: 'text-2xl font-medium my-4 font-serif',
-  section: 'text-1xl my-2',
+  header: "text-4xl font-medium my-4 font-serif",
+  section: "text-1xl my-2",
+  ul: "pl-5 mt-2 space-y-1 list-disc list-inside",
+  ol: "pl-5 mt-2 space-y-4 list-decimal list-inside text-black dark:text-gray-400",
 };
 
 export default styles;
